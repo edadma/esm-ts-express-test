f90c2ebaa96d980747f3552d18de8030826e1528
Edward A Maxedon <edadma@gmail.com>
2025-01-31 15:23:43 -0500

    SC-1112 added missing tsconfig settings

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,11 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT ?? 8000
 
-const asdf = 2345
-
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello World')
 })
 
